b7234a575714c034e648084e86bb5caeb4dbbb83
akylove <akyama.vieira@rr.sebrae.com.br>
2024-10-11 15:58:07 -0400

    first commit

---
+++ src/test/java/com/akyamasvieira/java_restfull_dio/JavaRestfullDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akyamasvieira.java_restfull_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestfullDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
